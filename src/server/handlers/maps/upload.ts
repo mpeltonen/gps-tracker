f0082dbf0255fbeea0f959c3f3b30f4477e48d9e
Mikko Peltonen <mikko.peltonen@reaktor.com>
2021-10-14 20:27:04 +0300

    Extract OCAD kmz file to upload directory

--- src/server/handlers/maps/upload.ts
+++ src/server/handlers/maps/upload.ts
@@ -2,6 +2,8 @@ import multer from "multer";
 import { NextFunction, Request, Response } from "express";
 import fs from "fs";
 import { MapUploadResponse } from "../../../shared/schemas";
+import * as unzipper from "unzipper";
+import { parseStringPromise } from "xml2js";
 
 const upload = multer({
   dest: "uploads",
@@ -12,7 +14,7 @@ const upload = multer({
 
 export const mapUploadHandlers = [
   upload.single("mapFile"),
-  (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const { file: mapFile } = req;
 
     const errorResponse = (msg: string): MapUploadResponse => ({ error: msg });
@@ -22,7 +24,7 @@ export const mapUploadHandlers = [
       if (!mapFile) {
         return res.status(400).json(errorResponse(`No file named 'mapFile'`));
       }
-
+      await handleFile(mapFile);
       res.json(okResponse(mapFile.originalname));
     } catch (err: unknown) {
       next(err);
@@ -33,3 +35,50 @@ export const mapUploadHandlers = [
     }
   },
 ];
+
+const handleFile = async (mapFile: Express.Multer.File) => {
+  const unzipDir = `uploads/maps`;
+  if (!fs.existsSync(unzipDir)) {
+    fs.mkdirSync(unzipDir, { recursive: true });
+  }
+  console.log(`Unzipping map file to ${unzipDir}...`);
+  await fs
+    .createReadStream(mapFile.path)
+    .pipe(unzipper.Parse())
+    .on("entry", async (entry) => {
+      const fileName = entry.path;
+      console.log(`Processing ${fileName}`);
+      if (fileName === "files/tile_0_0.jpg") {
+        entry.pipe(fs.createWriteStream(`${unzipDir}/${mapFile.filename}.jpg`));
+      } else if (fileName === "doc.kml") {
+        const metadata = transformKmlToJsonMetadata(await entry.buffer());
+        await fs.writeFileSync(`${unzipDir}/${mapFile.filename}.json`, JSON.stringify(metadata, null, 2));
+        entry.autodrain();
+      } else {
+        entry.autodrain();
+      }
+    })
+    .promise();
+  console.log(`Done.`);
+};
+
+const transformKmlToJsonMetadata = async (kmlAsStr: string) => {
+  const kmlAsJson = await parseStringPromise(kmlAsStr);
+  const groundOverlay = kmlAsJson?.kml?.Folder?.[0]?.GroundOverlay;
+  if (!groundOverlay) {
+    throw Error(`No GroundOverlay found`);
+  }
+  const latLonBox = groundOverlay[0]?.LatLonBox?.[0];
+  if (!latLonBox) {
+    throw Error(`No LatLonBox found`);
+  }
+  return {
+    latLonBox: {
+      north: parseFloat(latLonBox.north[0]),
+      south: parseFloat(latLonBox.south[0]),
+      east: parseFloat(latLonBox.east[0]),
+      west: parseFloat(latLonBox.west[0]),
+      rotation: parseFloat(latLonBox.rotation[0]),
+    },
+  };
+};
